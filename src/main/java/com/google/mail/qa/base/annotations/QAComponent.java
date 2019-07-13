da7f177a190bdd2006b518b828d62ff22d088b44
Mikita Yafimuk <efimukn@gmail.com>
2019-07-13 10:26:08 +0300

    Added spring support

---
+++ src/main/java/com/google/mail/qa/base/annotations/QAComponent.java
@@ -0,0 +1,20 @@
+package com.google.mail.qa.base.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Lazy
+@Component
+public @interface QAComponent
+{
+	String value() default "gmail";
+}
