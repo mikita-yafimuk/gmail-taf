ddb67411e25d1d65d22a89840e3633428107b372
Mikita Yafimuk <efimukn@gmail.com>
2019-07-13 11:22:04 +0300

    Added spring support

--- src/main/java/com/google/mail/qa/base/steps/AbstractStepDefenition.java
+++
@@ -1,5 +0,0 @@
-package com.google.mail.qa.base.steps;
-
-public abstract class AbstractStepDefenition
-{
-}
---
+++ src/main/java/com/google/mail/qa/base/steps/AbstractStepDefinition.java
@@ -0,0 +1,8 @@
+package com.google.mail.qa.base.steps;
+
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration("classpath:applicationContext.xml")
+public abstract class AbstractStepDefinition
+{
+}
