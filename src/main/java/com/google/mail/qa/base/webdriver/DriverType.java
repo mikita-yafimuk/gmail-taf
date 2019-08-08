b35a7b67f0fe54b2a6dcd60af49e9fc51d8ca74a
Mikita Yafimuk <efimukn@gmail.com>
2019-08-08 23:17:40 +0300

    Implement additional functionality for DriverType enum (add string + getters)

--- src/main/java/com/google/mail/qa/base/webdriver/DriverType.java
+++ src/main/java/com/google/mail/qa/base/webdriver/DriverType.java
@@ -1,10 +1,36 @@
 package com.google.mail.qa.base.webdriver;
 
+import com.google.mail.qa.base.exceptions.BaseException;
+
 /**
  * @author Mikita Yafimuk
  */
 public enum DriverType
 {
-	CHROME,
-	FIREFOX
+	CHROME("Chrome"),
+	FIREFOX("Firefox");
+
+	private String driverType;
+
+	DriverType(String driverType)
+	{
+		this.driverType = driverType;
+	}
+
+	public String getDriverType()
+	{
+		return this.driverType;
+	}
+
+	public static DriverType getDriverByName(String driverName)
+	{
+		for (DriverType driverType : DriverType.values())
+		{
+			if (driverType.getDriverType().equalsIgnoreCase(driverName))
+			{
+				return driverType;
+			}
+		}
+		throw new BaseException("Unknown driver name: " + driverName);
+	}
 }
