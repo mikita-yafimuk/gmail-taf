1320468d11f639b03e2c674d9654dd35f174e6a8
Mikita Yafimuk <efimukn@gmail.com>
2019-08-21 21:07:35 +0300

    Add properties manager to abstract step definition

--- src/main/java/com/google/mail/qa/base/steps/AbstractStepDefinition.java
+++ src/main/java/com/google/mail/qa/base/steps/AbstractStepDefinition.java
@@ -1,8 +1,13 @@
 package com.google.mail.qa.base.steps;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+
+import com.google.mail.qa.base.utilities.PropertiesManager;
 
 @ContextConfiguration("classpath:applicationContext.xml")
 public abstract class AbstractStepDefinition
 {
+	@Autowired
+	protected PropertiesManager propertiesManager;
 }
